b22d1abb1c82e6ecc828215f1b24ff5141833442
Par-zy <j.davi4242@gmail.com>
2021-06-21 00:32:15 -0300

    harmonize colors

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Container(
+    home: Scaffold(
+        body: Container(
       color: Colors.purple,
-    ),
+      child: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          color: Colors.green,
+        ),
+      ),
+    )),
   ));
 }
